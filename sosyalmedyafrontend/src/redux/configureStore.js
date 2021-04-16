8599fa07e9820c852c9273ab7a4aca894397d40f
aberimen <berimen63@gmail.com>
2021-04-16 23:25:47 +0300

    06-kullanıcı bilgileri tarayıca localstorage eklendi

--- sosyalmedyafrontend/src/redux/configureStore.js
+++ sosyalmedyafrontend/src/redux/configureStore.js
@@ -1,19 +1,31 @@
 import { createStore } from 'redux';
 import authReducer from './authReducer';
 
-const loggedInState = {
-    isLoggedIn: false,
-    username: undefined,
-    displayName: undefined,
-    image: undefined,
-    password: undefined
-};
-
 
 const configureStore = () => {
+    const authDataLocalStorage = localStorage.getItem('auth-data');
 
-    return createStore(authReducer, loggedInState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    let stateInLocalStorage = {
+        isLoggedIn: false,
+        username: undefined,
+        displayName: undefined,
+        image: undefined,
+        password: undefined
+    };
 
+    if (authDataLocalStorage) {
+        try {
+            stateInLocalStorage = JSON.parse(authDataLocalStorage);
+        } catch (error) { }
+    }
+
+    const store = createStore(authReducer, stateInLocalStorage, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+    store.subscribe(() => {
+        localStorage.setItem('auth-data', JSON.stringify(store.getState()));
+    });
+
+    return store;
 }
 
 export default configureStore;
