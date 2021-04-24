88ba4f9b9fe952ec2c95f627b1d0b20809846aaf
aberimen <berimen63@gmail.com>
2021-04-25 01:32:32 +0300

    46- Post'ları default olarak sıralı getirme

--- sosyalmedya/src/main/java/com/aberimen/sosyalmedya/post/PostController.java
+++ sosyalmedya/src/main/java/com/aberimen/sosyalmedya/post/PostController.java
@@ -5,6 +5,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +28,7 @@ public class PostController {
 	}
 	
 	@GetMapping("/api/posts")
-	public Page<Post> getPosts(Pageable pageable){
+	public Page<Post> getPosts(@PageableDefault(sort = "id",direction = Direction.DESC ) Pageable pageable){
 		
 		return postService.getPosts(pageable);
 	}
