347f07bddbf42abb1b672816cf873fd17521b483
aberimen <berimen63@gmail.com>
2021-04-26 01:08:34 +0300

    50- timeago.js kütüphanesi ile Post gönderildiği tarihi anlamlı biçimde gösterilmesi.

--- sosyalmedyafrontend/src/components/PostItem.js
+++ sosyalmedyafrontend/src/components/PostItem.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ProfileImage from './ProfileImage';
+import { format } from 'timeago.js';
+import { useTranslation } from 'react-i18next';
 
 const PostItem = (props) => {
 
-    const { content, user } = props.post;
+    const { content, user, timestamp } = props.post;
     const { username, firstName, lastName } = user;
 
+    const {i18n} = useTranslation();
+
+    const formattedDate = format(timestamp,i18n.language);
+
+
+    format
     return (
         <div className="mt-2">
             <div className=" card p-1">
@@ -16,7 +24,7 @@ const PostItem = (props) => {
 
                     <div className="flex-fill m-auto">
                         <Link to={`/user/${username}`} className="text-dark" >
-                            <h6 className="card-title ml-2" >{`${firstName} ${lastName} `}<small class="text-muted">@{username}</small></h6>
+                            <h6 className="card-title ml-2" >{`${firstName} ${lastName} `}<small className="text-muted">@{username}</small></h6>
                         </Link>
                     </div>
                 </div>
@@ -25,7 +33,7 @@ const PostItem = (props) => {
                     <p className="card-text">
                         {content}
                     </p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p className="card-text"><small className="text-muted">{formattedDate}</small></p>
                 </div>
             </div>
         </div>
