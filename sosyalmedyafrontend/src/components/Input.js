830e8deb45575d58e507f667a67b986af3a4f6ef
aberimen <berimen63@gmail.com>
2021-05-01 17:41:26 +0300

    61- Attachment: Post'a image ekleme kısmı eklendi

--- sosyalmedyafrontend/src/components/Input.js
+++ sosyalmedyafrontend/src/components/Input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const Input = (props) => {
-    const { label, error, name, onChange, type, defaultValue } = props;
+    const { id, label, error, name, onChange, type, defaultValue } = props;
     let className = "form-control";
     if (type === "file") {
         className += "-file";
@@ -12,7 +12,7 @@ const Input = (props) => {
     return (
         <div className="form-group">
             <label>{label}</label>
-            <input className={className} name={name} type={type} onChange={onChange}
+            <input id={id} className={className} name={name} type={type} onChange={onChange}
                 defaultValue={defaultValue} />
             <div className="invalid-feedback">{error}</div>
         </div>
--- sosyalmedyafrontend/src/components/PostSubmit.js
+++ sosyalmedyafrontend/src/components/PostSubmit.js
@@ -4,12 +4,14 @@ import { sendPost } from '../api/apiCall';
 import { useApiProgress } from '../api/ApiProgress';
 import ButtonWithProgress from './ButtonWithProgress';
 import ProfileImage from './ProfileImage';
+import Input from './Input';
 
 const PostSubmit = () => {
 
     const [focused, setFocused] = useState(false); //text area aktifliğini tutan state
     const [post, setPost] = useState("");
     const [validationError, setValidationError] = useState({});
+    const [newImage, setNewImage] = useState();
 
     const pendingApiCall = useApiProgress('post', '/api/posts');
 
@@ -38,11 +40,29 @@ const PostSubmit = () => {
         }
     };
 
+    const onChangeFile = (event) => {
+        if (event.target.files.length < 1) {
+            return;
+        }
+        const file = event.target.files[0];
+        const fileReader = new FileReader();
+        fileReader.onloadend = () => {
+            setNewImage(fileReader.result);
+            setValidationError({ ...validationError, image: undefined }); //başka dosya seçildiğinde inputtaki hatayı silmesi
+        };
+        fileReader.readAsDataURL(file);
+    };
+
     let className = "form-control";
 
     if (validationError.content) {
         className += " is-invalid";
     }
+
+    const onClickCloseImage = () =>{
+        setNewImage();
+        document.getElementById('image').value= null;
+    };
 
     return (
         <div>
@@ -51,16 +71,29 @@ const PostSubmit = () => {
                 <ProfileImage user={user} style={{ width: "60px", height: "60px" }} className="rounded-circle border" />
 
                 <div className="p-3 text-right flex-fill">
-                    
+
                     <textarea className={className} rows={focused ? "3" : "1"}
                         onFocus={() => setFocused(true)} onChange={(event) => onChange(event)} value={post} />
                     <div class="invalid-feedback"> {validationError.content}</div>
+
+                    <Input id="image" type="file" onChange={onChangeFile} />
+                    <div className="position-relative" style={{display: newImage ? "block" : "none"}}>
+                        <button type="button" 
+                        className="close position-absolute text-white rounded-circle bg-secondary" 
+                        style={{ right: "1rem", top: "1rem", width: "2rem" ,height:"2rem"}} 
+                        aria-label="Close"
+                        onClick={onClickCloseImage}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <img class="img-fluid img-thumbnail" src={newImage} />
+                    </div>
+
 
                     <ButtonWithProgress
                         text="Post"
                         className="btn btn-primary mt-2 "
                         pendingApiCall={pendingApiCall}
-                        disabled={post.trim() === "" || pendingApiCall}
+                        disabled={newImage ? pendingApiCall : post.trim() === "" || pendingApiCall}
                         onClick={onClickPost} />
 
                 </div>
